25f81768b4f6433cb6b46970b8856dfb313e8b39
Erfan <Erfan@DESKTOP-UD5O3N6>
2019-01-08 13:08:11 +0330

    Add project files.

---
+++ ConsoleApp7/RandomNumber.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameAlon
+{
+    class RandomNumber
+    {
+        private static Random Random = new Random();
+        public static string RandomGenerator()
+        {
+            string[] RandomNumberArray = new string[4];
+            byte Counter = 0;
+            do
+            {
+                {
+                    RandomNumberArray[Counter] = Convert.ToString(Random.Next(0, 9));
+                    Counter++;
+                } while (Counter < 3) ;
+            } while (RandomNumberArray[0] == RandomNumberArray[1] || RandomNumberArray[0] == RandomNumberArray[2] || RandomNumberArray[0] == RandomNumberArray[3] || RandomNumberArray[1] == RandomNumberArray[2] || RandomNumberArray[1] == RandomNumberArray[3] || RandomNumberArray[2] == RandomNumberArray[3]);
+            string Result = RandomNumberArray[0] + RandomNumberArray[1] + RandomNumberArray[2] + RandomNumberArray[3];
+            return Result;
+        }
+    }
+}
---
+++ ConsoleApp7/UserDetail.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NetTelegramBotApi.Types;
+
+namespace GameAlon
+{
+    public class Person
+    {
+        internal string Name = null;
+        internal string UserName = null;
+        internal long ID;
+        internal short Score;
+        internal string ComputerGuessNumber = null;
+        internal byte Counter = 0;
+        //internal bool UserPlayedGuessGame = false;
+        public Person(string Name, string UserName, long ID)
+        {
+            this.Name = Name;
+            this.UserName = UserName;
+            this.ID = ID;
+            Score = 0;
+        }
+    }
+
+    //public class ProfileGuessGame
+    //{
+    //    private short PlusScore = 0;
+    //    public string ComputerGuessNumber = null;
+    //    public byte Counter = 0;
+    //    
+    //    
+    //    
+    //    
+    //    
+    //    
+    //    public ProfileGuessGame(short PlusScore) //When Our User Win The Game 
+    //    {
+    //        this.PlusScore = PlusScore;
+    //        Counter = 0;
+    //        ComputerGuessNumber = null;
+    //    }
+    //    public ProfileGuessGame() //When Our User Lose The Game
+    //    {
+    //        if (PlusScore > 0)
+    //        {PlusScore--;}
+    //        Counter = 0;
+    //        ComputerGuessNumber = null;
+    //    }
+    //    public ProfileGuessGame(string ComputerGuessNumber) //When Our User Start The Game
+    //    {
+    //        PlusScore = 0;
+    //        Counter = 0;
+            //  this.ComputerGuessNumber = ComputerGuessNumber;
+    //}   }
+//}
+    //public ProfileGuessGame(byte Counter) //When Our User Say Wrong Answer 
+    //{
+    //    this.Counter -= Counter;
+    //}
+}
